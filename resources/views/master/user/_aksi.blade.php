a9b7affd9104d79f06bc0a7e256cc355e9636c58
Ade Prast <adecma18@gmail.com>
2016-12-23 10:47:36 +0800

    group aksi button

--- resources/views/master/user/_aksi.blade.php
+++ resources/views/master/user/_aksi.blade.php
@@ -1,5 +1,7 @@
 {!! Form::open(['url' => $form_url, 'method' => 'delete', 'class' => 'ask-delete']) !!}
-	<a href="{{ $edit_url }}" class="btn btn-info btn-xs"><i class="fa fa-edit"></i> Edit</a>
+	<div class="btn-group">
+		<a href="{{ $edit_url }}" class="btn btn-info btn-xs"><i class="fa fa-edit"></i></a>
 
-	<button class="btn btn-danger btn-xs" type="submit"><i class="fa fa-trash"></i> Hapus</button>
+		<button class="btn btn-danger btn-xs" type="submit"><i class="fa fa-trash"></i></button>
+	</div>
 {!! Form::close() !!}
